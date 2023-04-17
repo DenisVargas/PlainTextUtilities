dd12d923d74b286d16cea9576624010e37ee0f55
Denis Vargas Rivero <denis.vargasrivero@outlook.com>
2023-04-16 23:49:04 -0300

    First Version
    Adds a simple utility that breaks down a large line into multiple lines. Maximun characters are hardcoded to 80.

--- src/extension.ts
+++ src/extension.ts
@@ -4,22 +4,63 @@ import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+// export function activate(context: vscode.ExtensionContext) {
+
+// 	// Use the console to output diagnostic information (console.log) and errors (console.error)
+// 	// This line of code will only be executed once when your extension is activated
+// 	console.log('Congratulations, your extension "PTUtils" is now active!');
+
+// 	// The command has been defined in the package.json file
+// 	// Now provide the implementation of the command with registerCommand
+// 	// The commandId parameter must match the command field in package.json
+// 	let disposable = vscode.commands.registerCommand('TextUtils.helloWorld', () => {
+// 		// The code you place here will be executed every time your command is executed
+// 		// Display a message box to the user
+// 		vscode.window.showInformationMessage('Hello World from PlainTextUtils!');
+// 	});
+
+// 	context.subscriptions.push(disposable);
+// }
+
 export function activate(context: vscode.ExtensionContext) {
+	console.log('Extention "TextUtils" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "PTUtils" is now active!');
+    let disposable = vscode.commands.registerCommand('TextUtils.WrapLine', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const document = editor.document;
+            const selections = editor.selections;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('PTUtils.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from PlainTextUtils!');
-	});
+            editor.edit((editBuilder) => {
+                selections.forEach((selection) => {
+                    const text = document.getText(selection);
+                    const wrappedText = wrapText(text);
+                    editBuilder.replace(selection, wrappedText);
+                });
+            });
+        }
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
+}
+
+function wrapText(text: string, lineWidth: number = 80): string {
+    const words = text.split(' ');
+    let line = '';
+    const lines = [];
+
+    words.forEach((word) => {
+        if ((line + ' ' + word).length > lineWidth) {
+            lines.push(line);
+            line = word;
+        } else {
+            line += (line.length > 0 ? ' ' : '') + word;
+        }
+    });
+
+    lines.push(line);
+
+    return lines.join('\n');
 }
 
 // This method is called when your extension is deactivated
